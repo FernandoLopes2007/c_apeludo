7f06b58932b341f09e605d16b8003d51d5c646b5
Fernando Lopes <fernando.lopes210607@gmail.com>
2026-04-06 09:18:45 -0300

    Update 5.c

--- lista/lista/5.c
+++ lista/lista/5.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
 int main(){
-    float cobre, zinco;
+    float latao, cobre, zinco;
+
+    printf("Digite a quantidade de latao:\n");
+    scanf("%f", &latao);
+
+    cobre = 70.0 / 100 * latao;
+    zinco = 30.0 / 100 * latao;
+
+    printf("Quantidade de cobre e zinco necessarios para fazer %.2f de latao:\nCobre: %.2f\nZinco : %f\n", latao, cobre, zinco);
+    return 0;
 }

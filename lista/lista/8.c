d8d13dc552988f2a947aa0d2e1772e88e4c04875
Fernando Lopes <fernando.lopes210607@gmail.com>
2026-04-06 08:48:01 -0300

    Update 8.c

--- lista/lista/8.c
+++ lista/lista/8.c
@@ -5,15 +5,17 @@ int main(){
     scanf("%d%d%d", &nota1, &nota2, &nota3);
 
     media = (nota1 + nota2 +nota3)/3;
-    printf("Media final: %d", media);
+    printf("Media final: %d\n", media);
 
     if(media >=7){
         printf("Aprovado");
     }
-    else if(media 3<= || <7){
+    else if(media <=3  || media <7){
         printf("Recuperacao");
     }
     else{
         printf("Reprovado");
     }
+
+    return 0;
 }

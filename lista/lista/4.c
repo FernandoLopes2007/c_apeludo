32cc2843796e461aede2a204a830fcc401a7838e
Fernando Lopes <fernando.lopes210607@gmail.com>
2026-04-06 08:43:19 -0300

    Update 4.c

--- lista/lista/4.c
+++ lista/lista/4.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 
 int main(){
-    float valor_consumidor, imposto, lucro, custo;
+    float valor_consumidor, imposto, lucro;
 
+    printf("Qual e o valor do carro?\n");
+    scanf("%f", &valor_consumidor);
     
-    lucro = 12.0 / 100  * custo;
-    imposto = 45.0 / 100 * custo;
+    lucro = 12.0 / 100  * valor_consumidor;
+    imposto = 45.0 / 100 * valor_consumidor;
     
+    printf("Valor do carro: %.2f\nValor em imposto: %.2f\nLucro do distribuidor: %.2f.", valor_consumidor, imposto, lucro);
+    return 0;
 }

7ffb78a8c89714e4d891daa394b7f3523f6ad439
Fernando Lopes <fernando.lopes210607@gmail.com>
2026-04-06 09:03:23 -0300

    Update 10.c

--- lista/lista/10.c
+++ lista/lista/10.c
@@ -1,13 +1,22 @@
 #include <stdio.h>
 
 int main(){
-    int velMax, velMoto, multa;
+    int velMax, velMoto, multa, velAcima;
     printf("Digite a velocidade maxima da avenida:\n");
     scanf("%d", &velMax);
     printf("Digite a velocidade que o motorista estava:\n");
     scanf("%d", &velMoto);
 
-    if(){
+     velAcima = velMoto - velMax;
 
+    if(velAcima < 11){
+        printf("Multa de 50 reais por ultrapassar %dkm/h da velocidade maxima de %dkm/h.\n", velAcima, velMax);
     }
+    else if(velAcima > 11 && velAcima < 31){
+        printf("Multa de 100 reais por ultrapassar %dkm/h da velocidade maxima de %dkm/h.\n", velAcima, velMax);
+    }
+    else{
+        printf("Multa de 200 reais por ultrapassar %dkm/h da velocidade maxima de %dkm/h.\n", velAcima, velMax);
+    }
+    return 0;
 }
